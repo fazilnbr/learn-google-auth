9b4f4db0938e4add640ce6cc870566f82038226b
FASIL <fasilkp2000@gmail.com>
2023-01-19 11:39:31 +0530

    compleeted
    compleete google auth project and there is some error with loger and i ignore loger

--- cmd/go-auth-2/main.go
+++ cmd/go-auth-2/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"google-auth/internal/configs"
+	"google-auth/internal/loger"
+	"google-auth/internal/services"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Initialize Viper across the application
+	configs.InitializeViper()
+
+	// Initialize Logger across the application
+	loger.InitializeZapCustomLogger()
+
+	// Initialize Oauth2 Services
+	services.InitializeOAuthGoogle()
+
+	// Routes for the application
+	http.HandleFunc("/", services.HandleMain)
+	http.HandleFunc("/login-gl", services.HandleGoogleLogin)
+	http.HandleFunc("/callback-gl", services.CallBackFromGoogle)
+
+	// loger.Log.Info("Started running on http://localhost:" + viper.GetString("port"))
+	fmt.Println("welcome")
+	log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+}
---
+++ internal/services/google.go
@@ -0,0 +1,99 @@
+package services
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"google-auth/internal/loger"
+
+	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	oauthConfGl = &oauth2.Config{
+		ClientID:     "",
+		ClientSecret: "",
+		RedirectURL:  "http://localhost:9090/callback-gl",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	oauthStateStringGl = ""
+)
+
+/*
+InitializeOAuthGoogle Function
+*/
+func InitializeOAuthGoogle() {
+	oauthConfGl.ClientID = viper.GetString("google.clientID")
+	oauthConfGl.ClientSecret = viper.GetString("google.clientSecret")
+	oauthStateStringGl = viper.GetString("oauthStateString")
+}
+
+/*
+HandleGoogleLogin Function
+*/
+func HandleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	HandleLogin(w, r, oauthConfGl, oauthStateStringGl)
+}
+
+/*
+CallBackFromGoogle Function
+*/
+func CallBackFromGoogle(w http.ResponseWriter, r *http.Request) {
+	// loger.Log.Info("Callback-gl..")
+
+	state := r.FormValue("state")
+	// loger.Log.Info(state)
+	if state != oauthStateStringGl {
+		// loger.Log.Info("invalid oauth state, expected " + oauthStateStringGl + ", got " + state + "\n")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	code := r.FormValue("code")
+	// loger.Log.Info(code)
+
+	if code == "" {
+		// loger.Log.Warn("Code not found..")
+		w.Write([]byte("Code Not Found to provide AccessToken..\n"))
+		reason := r.FormValue("error_reason")
+		if reason == "user_denied" {
+			w.Write([]byte("User has denied Permission.."))
+		}
+		// User has denied access..
+		// http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	} else {
+		token, err := oauthConfGl.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			loger.Log.Error("oauthConfGl.Exchange() failed with " + err.Error() + "\n")
+			return
+		}
+		// loger.Log.Info("TOKEN>> AccessToken>> " + token.AccessToken)
+		// loger.Log.Info("TOKEN>> Expiration Time>> " + token.Expiry.String())
+		// loger.Log.Info("TOKEN>> RefreshToken>> " + token.RefreshToken)
+
+		resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + url.QueryEscape(token.AccessToken))
+		if err != nil {
+			// loger.Log.Error("Get: " + err.Error() + "\n")
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		defer resp.Body.Close()
+
+		response, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// loger.Log.Error("ReadAll: " + err.Error() + "\n")
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+
+		// loger.Log.Info("parseResponseBody: " + string(response) + "\n")
+
+		w.Write([]byte("Hello, I'm protected\n"))
+		w.Write([]byte(string(response)))
+		return
+	}
+}
